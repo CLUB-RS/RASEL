6d5af61775c211409120001b156c1e04ff2e91df
CLUB-RS <132224455+CLUB-RS@users.noreply.github.com>
2023-07-24 22:15:55 +0600

    IP.py

--- IP.py
+++ IP.py
@@ -1,10 +1,12 @@
 import requests
 import os
 import pprint
+
+def ip():
+    ip = str(input("\n\n\nEnter IP Address: "))
+    url = f"https://ipapi.co/{ip}/json/"
+    r = requests.get(url)
+    pprint.pprint(r.json())
+
 while True:
-	def ip():
-		ip=str(input("\n\n\nEnter Ip Address :  "))
-		url=f"https://ipapi.co/{ip}/json/"
-		r=requests.get(url)
-		pprint.pprint(r.json())
-	ip()
+    ip()
